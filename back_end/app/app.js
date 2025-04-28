735225c6cb472466d41caa2cd69c15afa82aeb15
Charlotte-82 <charlotteregis1@gmail.com>
2025-04-28 11:20:14 +0200

    postman second tests => corrections

--- back_end/app/app.js
+++ back_end/app/app.js
@@ -2,9 +2,15 @@ const express = require("express");
 const app = express();
 const sequelize = require("../config/database");
 const artisanRoutes = require("../routes/artisanRoutes");
+const villeRoutes = require("../routes/villeRoutes");
+const specialiteRoutes = require("../routes/specialiteRoutes");
+const categorieRoutes = require("../routes/categorieRoutes");
 
 app.use(express.json());
 app.use("/api/artisans", artisanRoutes);
+app.use("/api/villes", villeRoutes);
+app.use("/api/specialites", specialiteRoutes);
+app.use("/api/categories", categorieRoutes);
 
 (async () => {
   try {
@@ -14,7 +20,7 @@ app.use("/api/artisans", artisanRoutes);
 
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () =>
-      console.log(` Le serveur est lancé sur http://localhost:${PORT}`)
+      console.log(`Le serveur est lancé sur http://localhost:${PORT}`)
     );
   } catch (error) {
     console.error("Erreur de démarrage serveur :", error);
--- back_end/middlewares/artisanValidator.js
+++ back_end/middlewares/artisanValidator.js
@@ -1,15 +1,24 @@
 const { body } = require("express-validator");
 
 exports.validateArtisan = [
-  body("nom").isString().notEmpty().withMessage("Le nom est obligatoire"),
-  body("email").isEmail().withMessage("Email invalide"),
-  body("note")
+  body("artisan_nom")
+    .isString()
+    .notEmpty()
+    .withMessage("Le nom est obligatoire"),
+  body("artisan_email").isEmail().withMessage("Email invalide"),
+  body("artisan_note")
     .optional()
     .isFloat({ min: 0, max: 5 })
     .withMessage("La note doit être entre 0 et 5"),
-  body("top").optional().isBoolean().withMessage("Top doit être un booléen"),
-  body("site").optional().isURL().withMessage("Site doit être une URL valide"),
-  body("specialite").optional().isString(),
-  body("ville").optional().isString(),
-  body("aPropos").optional().isString(),
+  body("artisan_top")
+    .optional()
+    .isBoolean()
+    .withMessage("Top doit être un booléen"),
+  body("artisan_site")
+    .optional()
+    .isURL()
+    .withMessage("Site doit être une URL valide"),
+  body("specialite_libelle").optional().isString(),
+  body("ville_nom").optional().isString(),
+  body("artisan_apropos").optional().isString(),
 ];
