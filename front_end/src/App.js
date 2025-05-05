004a6be3bacca0488de18e5f73bd256e4f6a7a1d
Charlotte-82 <charlotteregis1@gmail.com>
2025-05-05 20:41:01 +0200

    home dev

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "../src/components/Header";
+import CategoriePage from "./pages/CategoriePage";
 import Home from "../src/pages/Home";
 import FicheArtisan from "../src/pages/FicheArtisan";
 import Footer from "../src/components/Footer";
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/categoriepage" element={<CategoriePage />}></Route>
         <Route path="/ficheartisan" element={<FicheArtisan />}></Route>
         <Route path="/construction" element={<Construction />}></Route>
       </Routes>
--- front_end/src/pages/Home.js
+++ front_end/src/pages/Home.js
@@ -2,6 +2,9 @@ import React from "react";
 import "../assets/css/Home.css";
 import Chevron from "../assets/images/chevron-droit.png";
 import ArtisanMains from "../assets/images/ArtisanMains.jpg";
+import Boulanger from "../assets/images/boulanger.jpg";
+import Chauffe from "../assets/images/chauffagiste.jpg";
+import Chocolat from "../assets/images/chocolaterie.jpg";
 
 function Home() {
   return (
@@ -52,14 +55,190 @@ function Home() {
           ></img>
         </span>
       </div>
-      <div className="d-flex justify-content-between align-items-start px-3 py-2">
+
+      <hr className="hrTitre2" style={{ marginLeft: "6em" }} />
+      <h2 style={{ marginLeft: "3em" }}>Artisans du mois</h2>
+
+      <div
+        className="d-flex justify-content-between px-3 py-4"
+        style={{ gap: "20px" }}
+      >
+        {/* Top Artisan 1 */}
         <div
-          className="d-flex flex-column justify-items-start align-items-start"
-          style={{ marginLeft: "5em" }}
+          className="d-flex flex-column p-3"
+          style={{ width: "30%", height: "35em" }}
         >
-          <hr className="hrTitre2" />
-          <h2>Artisans du mois</h2>
+          <div
+            style={{
+              position: "relative",
+              display: "flex",
+              alignItems: "flex-start",
+              height: "100px",
+            }}
+          >
+            <img
+              src={Boulanger}
+              alt="mains de boualnger qui tiennent une baguette"
+              style={{
+                width: "100%",
+                height: "auto",
+                objectFit: "cover",
+                marginRight: "20px",
+                zIndex: 1,
+              }}
+            />
+
+            <div
+              style={{
+                position: "absolute",
+                top: "220%",
+                left: "60px",
+                height: "7em",
+                width: "89%",
+                borderLeft: "solid 2px #0074c7",
+                zIndex: 2,
+              }}
+            >
+              <h3
+                style={{
+                  zIndex: 1,
+                  textAlign: "right",
+                  paddingTop: "3em",
+                  color: "#00497c",
+                }}
+              >
+                Au Pain Chaud
+              </h3>
+              <p>À propos</p>
+              <button className="boutonDecouvrir">
+                Découvrir
+                <i
+                  className="bi bi-arrow-right"
+                  style={{ marginLeft: "2em" }}
+                ></i>
+              </button>
+            </div>
+          </div>
         </div>
+
+        {/* Top Artisan 2 */}
+        <div className="d-flex flex-column p-3" style={{ width: "30%" }}>
+          <div
+            style={{
+              position: "relative",
+              display: "flex",
+              alignItems: "flex-start",
+              height: "100px",
+            }}
+          >
+            <img
+              src={Chauffe}
+              alt="mains de chauffagiste qui règle une chaudière"
+              style={{
+                width: "100%",
+                height: "auto",
+                objectFit: "cover",
+                marginRight: "20px",
+                zIndex: 1,
+              }}
+            />
+
+            <div
+              style={{
+                position: "absolute",
+                top: "220%",
+                left: "60px",
+                height: "7em",
+                width: "89%",
+                borderLeft: "solid 2px #0074c7",
+                zIndex: 2,
+              }}
+            >
+              <h3
+                style={{
+                  zIndex: 1,
+                  textAlign: "right",
+                  paddingTop: "3em",
+                  color: "#00497c",
+                }}
+              >
+                Orville Salmons
+              </h3>
+              <p>À propos</p>
+              <button className="boutonDecouvrir">
+                Découvrir
+                <i
+                  className="bi bi-arrow-right"
+                  style={{ marginLeft: "2em" }}
+                ></i>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/* Top Artisan 3 */}
+        <div className="d-flex flex-column p-3" style={{ width: "30%" }}>
+          <div
+            style={{
+              position: "relative",
+              display: "flex",
+              alignItems: "flex-start",
+              height: "100px",
+            }}
+          >
+            <img
+              src={Chocolat}
+              alt="mains de chocolatier qui saupoudre des brisures de noisette sur une tablette"
+              style={{
+                width: "100%",
+                height: "auto",
+                objectFit: "cover",
+                marginRight: "20px",
+                zIndex: 1,
+              }}
+            />
+
+            <div
+              style={{
+                position: "absolute",
+                top: "220%",
+                left: "60px",
+                height: "7em",
+                width: "89%",
+                borderLeft: "solid 2px #0074c7",
+                zIndex: 2,
+              }}
+            >
+              <h3
+                style={{
+                  zIndex: 1,
+                  textAlign: "right",
+                  paddingTop: "3em",
+                  color: "#00497c",
+                }}
+              >
+                Chocolaterie Labbé
+              </h3>
+              <p>À propos</p>
+              <button className="boutonDecouvrir">
+                Découvrir
+                <i
+                  className="bi bi-arrow-right"
+                  style={{ marginLeft: "2em" }}
+                ></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginBottom: "2em",
+        }}
+      >
+        <button className="boutonBasPage">Voir tous les artisans</button>
       </div>
     </div>
   );
